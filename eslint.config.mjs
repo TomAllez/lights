7de754919c4d7b640efab69ed65a1aca046159cf
TomAllez <allezthomas@gmail.com>
2026-04-27 08:17:34 +0200

    fix(lint): ignore all dist

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import js from '@eslint/js';
 import ts from 'typescript-eslint';
 
 export default ts.config(
+  { ignores: ['**/dist/**'] },
   js.configs.recommended,
   ...ts.configs.recommended,
   {
